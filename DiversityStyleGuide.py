9529ff500408c049be638d1bec5b2f309de27f7f
eecoston <69772473+eecoston@users.noreply.github.com>
2020-09-19 18:04:46 -0700

    Fixed issue in URLtoGoogleDocJSONtoText
    Fixed issue that made the script fail when it ran into a non paragraph item. Now, it's set to only scrape paragraph items and ignore all others.

--- DiversityStyleGuide.py
+++ DiversityStyleGuide.py
@@ -1,14 +1,3 @@
-from __future__ import print_function
-import requests
-from bs4 import BeautifulSoup
-import json
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google_auth_oauthlib import flow
-
 class GoogleDocReferenceDivStyleGuide:
     def __init__(self,url):
         self.url = url
@@ -25,6 +14,7 @@ class GoogleDocReferenceDivStyleGuide:
         print('getting document ID ...')
         self.url =  self.url.replace('https://docs.google.com/document/d/','')
         self.url =  self.url.replace('/edit','')
+        self.url = self.url.replace('?usp=sharing','')
         print('document id: {}'.format( self.url))
         return  self.url
     
@@ -32,17 +22,14 @@ class GoogleDocReferenceDivStyleGuide:
     def Authenticate(self):
         extracted_text = []
         print("authenticating ...")
-        appflow = flow.InstalledAppFlow.from_client_secrets_file(r"YOUR API FILE PATH HERE",scopes=["https://www.googleapis.com/auth/drive.readonly"]
+        appflow = flow.InstalledAppFlow.from_client_secrets_file(r"C:\Users\Ethan Coston\Desktop\client_secret_883115423488-r97oa086lvgqembkqmkr34u04iqlso5a.apps.googleusercontent.com.json",scopes=["https://www.googleapis.com/auth/drive.readonly"]
         )
         launch_browser=True
-
         if launch_browser==False:
             appflow.run_local_server()
         else:
             appflow.run_console()
-
         credential = appflow.credentials
-        
         print("authentication complete\r\n\r\n")
         print("beginning google doc extract ...")
         service = build('docs', 'v1', credentials=credential)
@@ -53,34 +40,33 @@ class GoogleDocReferenceDivStyleGuide:
     
     
     def URLtoGoogleDocJSONtoText(self):
+        
         self.docIDfromURL()
         text = self.Authenticate()
+        print('Exporting text from Google Doc ...')
         article_as_dict = json.loads(text)
         text_as_dict = article_as_dict['body']['content']
         len(text_as_dict)
         text_as_list = []
         counter = 1
         while counter <= (len(text_as_dict)-1):
-            paragr = text_as_dict[counter]['paragraph']['elements']
-
-            lnpara = len(paragr)
-            if lnpara == 1:
-
-                text_as_list.append(paragr[0]['textRun']['content'])
-
-            else:
-                counter2 = 0
-                while counter2 <= (len(paragr)-1):
-
-                    text_as_list.append(paragr[counter2]['textRun']['content'])
-
-                    counter2 = counter2 + 1
+            if "paragraph" in text_as_dict[counter]:
+                paragr = text_as_dict[counter]['paragraph']['elements']
+                if len(paragr) == 1:
+                    text_as_list.append(paragr[0]['textRun']['content'])
+                else:
+                    counter2 = 0
+                    while counter2 <= (len(paragr)-1):
+                        if "textRun" in paragr[counter2]:
+                            text_as_list.append(paragr[counter2]['textRun']['content'])
+                        counter2 = counter2 + 1
             counter = counter + 1
         text_as_str = ''
         for x in text_as_list:
-            text_as_str += x
+             text_as_str += x
+        print('Export complete\r\n\r\n')
         self.text = text_as_str
-        return text_as_str
+        return self.text
     
     
     def ScrapeAndReference(self):
@@ -88,7 +74,6 @@ class GoogleDocReferenceDivStyleGuide:
         dictionary = self.StyleGuideScraper()
         self.URLtoGoogleDocJSONtoText()
         self.cleaningText()
-       
         self.referenceStyleGuide(dictionary)
         
         
@@ -99,23 +84,17 @@ class GoogleDocReferenceDivStyleGuide:
         style_guide
         html_soup = BeautifulSoup(style_guide.text, 'html.parser')
         glossary = html_soup.find('ul', attrs={"class": "glossaryList"})
-        
         dictionary = {}
         for lst in glossary.findAll("li"):
-
             tx_nme = lst.find('a')
             if '"' in tx_nme.get_text()[0]:
                 txt_def = str(tx_nme.get_text()[1:-1])
             else:
                 txt_def = str(tx_nme.get_text())
-
             defin = lst.find('div', attrs={"class": "glossary_itemdesc"})
-
-
             dictionary[txt_def] = defin.text
         print('Completed scrape\r\n\r\n')
         dictionary = self.cleanScrapedStyleguide(dictionary)
-        
         return dictionary
         
         
@@ -132,7 +111,6 @@ class GoogleDocReferenceDivStyleGuide:
                         split.remove('\xa0')
                     while('\xa0' in split):
                         split.remove('\xa0a')
-
             while("" in split) : 
                 split.remove("")
             while('\xa0' in split):
@@ -142,19 +120,17 @@ class GoogleDocReferenceDivStyleGuide:
             while('\x0b' in split):
                 split.remove('\x0b')
             split = '\r\n\r\n'.join(split)+'\r\n\r\n\r\n'
-
             dictionary[x] = split
         print('cleaned\r\n\r\n')
         return dictionary
 
 
     def cleaningText(self):
-        print('cleaning ...')
+        print('cleaning Google Doc text ...')
         self.commas()
         self.periods()
         self.quotes()
         print('cleaned\r\n\r\n')
-
         return ('cleaned')
         
         
@@ -175,7 +151,6 @@ class GoogleDocReferenceDivStyleGuide:
         
         
     def quotes(self):
-        
         self.text = self.text.replace('”','')
         self.text = self.text.replace('“','')
         print('removed quotes')
@@ -185,10 +160,8 @@ class GoogleDocReferenceDivStyleGuide:
         dictionary = var
         print('beginning reference ...\r\n\r\n\r\n')
         for x in dictionary:
-            
             if ' '+x+' ' in self.text:
                 location = self.text.find(x)
-
                 print('Location: {}\r\n\r\n{}: {}'.format(location,x,dictionary[x]))
         print('reference complete')
         return('completed\r\n\r\n')
